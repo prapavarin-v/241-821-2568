1730b4f7d6a88cc614566f82131372772ff97c5e
prapavarin-v <prapavarin.v@ku.th>
2025-12-30 11:14:15 +0700

    add js

---
+++ JS/main.js
@@ -0,0 +1,81 @@
+//console.log("Hello World");
+/*let fname = "John";
+console.log("Name:", fname);
+const PI = 3.14;
+
+let age = 30;
+let height = 5.9;
+
+fname="Alice";
+console.log("Hello " + fname);
+
+fname="Bob";
+PI=3.14159;
+console.log("Name: ",fname);
+console.log("Age: " ,age);
+console.log("Height: ", height);*/
+
+/*let number1='10';
+let number2='3';
+let result=number1+number2;
+console.log("ผลบวก:", result);*/
+
+/*let number1=10;
+let number2=20;
+let condition=number1<number2; // true
+console.log("condition:", condition);*/
+
+//ต้องเป็นจริงถึงเข้า condition
+/*
+let number1=3;
+let number2=5;
+if(number1>=number2){//เช็คหลายเงื่อนไข AND &&
+    console.log('this is if');
+}else if{
+    console.log('this is else if');
+}else{
+    console.log('this is else');
+}*/
+
+//คำนวน Grade ระวัง sytax!!!!
+/*let score=prompt("Enter your score:");
+ if(score>=80){
+    console.log('Grade A');
+ }else if(score>=70){
+    console.log('Grade B');
+ }else if(score>=60){
+    console.log('Grade C');
+ }else if(score>=50){
+    console.log('Grade D');
+ }else{
+    console.log('Grade F');
+ } */
+
+/*let number1=5
+let number2=10
+
+let condition1=(number1>0) || (number2>0)
+console.log("condition1:", condition1)
+
+let age=25
+let gender="female"
+if(age >= 18 && gender=="female"){
+    console.log("You are eligible to vote.");
+}*/
+
+/*let number1=20
+
+if(number1 %2 == 0){
+    console.log("Even number");
+}*/
+
+//Loop
+let conter=0;
+while(conter<=4){
+    conter+=1;
+    console.log("while:", conter);
+}
+
+for(let i=0; i<=4; i++){
+    console.log("for:", i);
+}
